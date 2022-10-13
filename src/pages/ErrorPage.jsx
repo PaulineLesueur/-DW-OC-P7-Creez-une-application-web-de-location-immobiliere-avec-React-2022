644475e4078b3a9868eee778ce27e2537f7e83e8
Pauline Lesueur <paulinelesueur93@gmail.com>
2022-10-13 15:03:47 +0200

    error page mobile version

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,21 @@
+import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function ErrorPage() {
     return (
-        <div>
+        <React.Fragment>
             <Header />
-            <h1>erreur 404</h1>
-        </div>
-    );
+            <main className="error-page">
+                <div className="error-page">
+                    <span>404</span>
+                    <p>Oups! La page que vous demandez n'existe pas.</p>
+                    <a href="/">Retourner sur la page d’accueil</a>
+                </div>
+            </main>
+            <Footer />
+        </React.Fragment>
+    )
 }
 
 export default ErrorPage;
