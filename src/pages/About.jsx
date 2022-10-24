ca340202e25203a911a39e55a8ac30d227d7fca0
Pauline Lesueur <paulinelesueur93@gmail.com>
2022-10-24 14:54:38 +0200

    import react and footer, add main and footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function About() {
     return (
-        <div>
+        <React.Fragment>
             <Header />
-            <h1>About</h1>
-        </div>
+            <main>about</main>
+            <Footer />
+        </React.Fragment>
     );
 }
 
