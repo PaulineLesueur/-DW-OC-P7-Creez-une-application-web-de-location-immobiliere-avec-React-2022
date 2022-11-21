3875326c5470096e5a09e4646a73c53d5bce5e48
Pauline Lesueur <paulinelesueur93@gmail.com>
2022-11-21 16:52:16 +0100

    clear

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Collapsible from "../components/Collapsible"
+import Collapsible from "../components/Collapsible";
 
 function About() {
     return (
